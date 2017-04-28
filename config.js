56755a2257f72e07e4c4a37c38256ed354ffd83d
jylei <jlei91@gmail.com>
2017-04-28 01:33:40 -0700

    added test db

--- config.js
+++ config.js
@@ -2,4 +2,6 @@ exports.DATABASE_URL =  process.env.DATABASE_URL ||
                         process.env.MONGODB_URI ||
                         'mongodb://localhost/notecard-app';
 
+exports.TEST_DATABASE_URL =  process.env.TEST_DATABASE_URL ||
+                            'mongodb://localhost/test-notecard-app';
 exports.PORT = process.env.PORT || 8080;
