80a58ef8efe028942983cde3c57679b5f4da073a
jylei <jlei91@gmail.com>
2017-05-03 03:00:20 -0700

    delete function

--- public/app.js
+++ public/app.js
@@ -47,34 +47,43 @@
 // }
 
 // getAndDisplayNoteCard();
+// "http://localhost:8080/"
+// "https://rocky-mesa-37949.herokuapp.com/"
 
-const BASE_URL = "https://rocky-mesa-37949.herokuapp.com/";
+const BASE_URL = "http://localhost:8080/";
 
 function getNotecardData(callback) {
-  const query = {
-    url: BASE_URL + 'notecards',
-    success: callback
-  }
-  return $.getJSON(query);
+    const query = {
+        url: BASE_URL + 'notecards',
+        success: callback
+    }
+    $.getJSON(query);
 }
 
 function displayNoteCard(data) {
     console.log(data);
     let notecardhtml = '';
-    if(data.length){
+    if (data.length) {
         data.forEach(notecard => {
             notecardhtml += `                            
-            <div class="col-md-6"><div class="panel panel-default"><div class="note-front"><div class="term">${notecard.title}</div></div></div></div>`;
+            <div class="col-md-6" id="f1_container"><div class = "delete-notecard hidden" data-id = "${notecard.id}">x</div><div id="f1_card" class="panel panel-default shadow"><div class="note-front ${notecard.color} front face"><div class="term" data-id = "${notecard.id}">${notecard.title}</div></div><div class="back face note-back">
+    <div class = "notecard-header">${notecard.category}</div>
+    <div class = "notecard-definition">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</div>
+    <div class = "notecard-tag">${notecard.tags}</div>
+  </div></div></div>
+             `;
         })
     }
     $('#profile-grid').append(notecardhtml);
 }
 
-
-
 $('.add-btn').on('click', function (e) {
     console.log('button clicked');
     renderModalContent();
+})
+
+$('.del-btn').on('click', function(){   
+    $('#profile-grid').find("div.delete-notecard").removeClass("hidden");
 })
 
 function renderModalContent() {
@@ -96,35 +105,64 @@ function renderModalContent() {
     $('#newnotecard').modal({ show: true });
 }
 
-
 function addCardData() {
-    $('.new-notecard-form').on('click', '.submit', function (e) {
-        console.log('submit note clicked');
-        let formInput = {
+    const colors = ["pink", "green", "yellow", "blue"];
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    $('.new-notecard-form').on('click', '.submit', function () {
+        let cardInput = {
             title: $('#title-input').val(),
             category: $('#category-input').val(),
-            definition: $('#definition-input').val()
+            definition: $('#definition-input').val(),
+            color: randomColor
         }
+            const tagArray = [];
         //tags optional
         if ($('#tag-input').val()) {
-            //fix to take val and put in arr
-            formInput.tags = $('#tag-input').val();
+            tagArray.push($('#tag-input').val());
+            cardInput.tags = tagArray;
         }
-
         $.ajax({
             type: 'POST',
             url: BASE_URL + 'notecards',
             processData: false,
-            data: JSON.stringify(formInput),
+            data: JSON.stringify(cardInput),
             dataType: "json",
             contentType: "application/json",
             success: function () {
                 location.reload();
             }
-        })
-        location.reload();
+        });
     });
-}
+};
 
-addCardData();
+function deleteCardData() {
+    $('#profile-grid').on('click', '.delete-notecard', function () {
+        console.log('delete clicked');
+        let testid = $(this).attr('data-id');
+        let delete_url = BASE_URL + `notecards/${testid}`;
+        $.ajax({
+            type: 'DELETE',
+            url: delete_url,
+            success: function () {
+                location.reload();
+            }
+        });
+    });
+};
+
 getNotecardData(displayNoteCard);
+deleteCardData();
+addCardData();
+
+//document ready to execute above -- getnotecarddata, call again after info is posted instead of location reload
+//function to open close modal when data submitted and new data loaded in
+//testing part - deck of cards that user can modify, define/modify order
+//keep track of correct/incorrect, have results(last 10 times,etc)
+
+
+//notecard by color - pink, green, blue, yellow - pastel colors
+//bulk creation, allow to make 10 at once(not priority)
+
+//finish api, color, flipping animation - priority
+//category on navbar and search 
+//color for branding, APP NAME!!
