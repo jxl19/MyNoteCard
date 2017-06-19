0733ee071a4937e1ac0fb883ecc8db5874c36d3c
jylei <jlei91@gmail.com>
2017-06-18 17:34:07 -0700

    config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.DATABASE_URL =  process.env.DATABASE_URL ||
                         process.env.MONGODB_URI ||
-                        'mongodb://localhost/notecard-app';
-                        // 'mongodb://jylei:pw123@ds123371.mlab.com:23371/notecard-app';
+                        'mongodb://localhost/notecard-app' ||
+                        'mongodb://jylei:pw123@ds123371.mlab.com:23371/notecard-app';
 
 exports.TEST_DATABASE_URL =  process.env.TEST_DATABASE_URL ||
                             'mongodb://localhost/test-notecard-app';
